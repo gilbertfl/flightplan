b799f64cb85894dbaaa20af30eccfe43ed7af6b8
Paul Bauer <paulb@moj.io>
2019-06-01 10:07:47 -0300

    add all cors, why is nothing working

--- bin/cli-server.js
+++ bin/cli-server.js
@@ -9,8 +9,8 @@ var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 
-//app.use(cors())
-//app.options('*', cors()) // include before other routes
+app.use(cors())
+app.options('*', cors()) // include before other routes
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -18,7 +18,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/api/config', async (req, res, next) => {
+app.get('/api/config', cors(), async (req, res, next) => {
   try {
     // Insert each website engine
     const engines = fp.supported().map((id) => {
@@ -45,7 +45,7 @@ app.get('/api/config', async (req, res, next) => {
   }
 })
 
-app.get('/api/search', async (req, res, next) => {
+app.get('/api/search', cors(), async (req, res, next) => {
   try {
     const {
       fromCity = '',
