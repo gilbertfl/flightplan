d19074f5d11daefe9f3ddce8efb2defa153af9af
Paul Bauer <paulb@moj.io>
2019-06-02 09:56:33 -0300

    path prefix

--- shared/paths.js
+++ shared/paths.js
@@ -1,6 +1,6 @@
 module.exports = {
-  credentials: 'config/accounts.txt',
-  oldCredentials: 'config/accounts.json',
-  database: 'db/database.sqlite3',
-  data: 'data'
+  credentials: (process.env.MOUNTPATH || '.') + '/config/accounts.txt',
+  oldCredentials: (process.env.MOUNTPATH || '.') + '/config/accounts.json',
+  database: (process.env.MOUNTPATH || '.') + '/db/database.sqlite3',
+  data: (process.env.MOUNTPATH || '.') + '/data'
 }
