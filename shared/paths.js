656cd90d3039d3a8ebd2c1647a9de9d481dabce7
Paul Bauer <paulb@moj.io>
2020-05-10 08:35:53 +1000

    update sql server url env name

--- shared/paths.js
+++ shared/paths.js
@@ -1,7 +1,7 @@
 module.exports = {
   credentials: (process.env.MOUNTPATH || '.') + '/config/accounts.txt',
   oldCredentials: (process.env.MOUNTPATH || '.') + '/config/accounts.json',
-  database: process.env.SQLCONNSTR || '*.database.windows.net',
+  database: process.env.SQLSERVERURL || '*.database.windows.net',
   databaseName: process.env.SQLDBNAME || 'flightplan', 
   databaseUser: process.env.SQLUSER || '', 
   databasePassword: process.env.SQLPASS || '', 
